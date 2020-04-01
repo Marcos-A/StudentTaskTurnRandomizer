b9c644d96f4bf9779c08ad5d46e5b2c34a24c248
Marcos Alcocer-Gil <marcosalcocer@gmail.com>
2020-04-01 15:40:35 +0200

    Added option to assign multiple tasks with different options for each of them.

--- StudentTaskTurnRandomizer.py
+++ StudentTaskTurnRandomizer.py
@@ -3,26 +3,10 @@
 
 import math
 import random
+import string
 import sys
 
-ASSIGN_TASK = True
-ASSIGN_TURN = True
-if len(sys.argv) == 3:
-    if sys.argv[2].lower() == 'task':
-        ASSIGN_TASK = True
-        ASSIGN_TURN = False
-    if sys.argv[2].lower() == 'turn':
-        ASSIGN_TASK = False
-        ASSIGN_TURN = True
-if len(sys.argv) == 4:
-    if sys.argv[3].lower() == 'task':
-        ASSIGN_TASK = True
-    if sys.argv[3].lower() == 'turn':
-        ASSIGN_TURN = True
-
-
-TOTAL_TASKS = 4
-STUDENTS_PER_TURN_RATIO = 4
+ALPHABET = list(string.ascii_uppercase)
 
 MP5_STUDENTS = ['Diana', 'Sara B', 'Anabel', 'Víctor', 'Sara D',
                 'Estefanía', 'Lidia', 'Carlos', 'Fátima', 'Aqsa',
@@ -33,6 +17,29 @@ MP10_STUDENTS = ['Diana', 'Sara B', 'Anabel', 'Víctor', 'Sara D',
                  'Estefanía', 'Lidia', 'Carlos', 'Fátima', 'Aqsa',
                  'Carmen', 'Ángela', 'Nayely', 'Dani', 'Mehdi',
                  'Mireia', 'Marc', 'Jan', 'Rosi']
+
+ASSIGN_TASK = True
+ASSIGN_TURN = True
+if len(sys.argv) == 3:
+    if sys.argv[2].lower() == 'task':
+        ASSIGN_TASK = True
+        ASSIGN_TURN = False
+    elif sys.argv[2].lower() == 'turn':
+        ASSIGN_TASK = False
+        ASSIGN_TURN = True
+if len(sys.argv) == 4:
+    if sys.argv[3].lower() == 'task':
+        ASSIGN_TASK = True
+        ASSIGN_TURN = False
+
+TOTAL_OPTIONS_PER_TASK = 4
+STUDENTS_PER_TURN_RATIO = 4
+
+TOTAL_TASKS = 2
+args_list = list(sys.argv)
+for arg in args_list:
+    if arg.isdigit():
+        TOTAL_TASKS = int(arg)
 
 
 """
@@ -77,39 +84,44 @@ def assign_students_to_list(original_students_list, turns_dict):
 
 
 """
-Create list of tasks
+Create list of options
 """
-def create_tasks_list():
-    tasks_list = []
+def create_options_list():
+    options_list = []
     i = 0
-    while i < TOTAL_TASKS:
-        tasks_list.append('Tasca ' + str(i+1))
+    while i < TOTAL_OPTIONS_PER_TASK:
+        options_list.append('Opció ' + ALPHABET[i])
         i += 1
 
-    return tasks_list
+    return options_list
 
 
 """
-Create dict of students with a randomly assigned task
+Create dict of students with a randomly assigned options per task
 """
-def distribute_tasks_between_students(task_list, original_students_list):
-    students_list = original_students_list.copy()
-    students_dict = {}
+def distribute_tasks_between_students(options_list, *original_students_list, **students_dict):
+    students_list = list(original_students_list)
 
     while students_list:
-        for task in task_list:
+        for task in options_list:
             if not students_list:
                 break
             else:
                 selected_student = random.choice(students_list)
-                students_dict[selected_student] = task
+
+                if selected_student not in students_dict.keys():
+                    students_dict[selected_student] = task
+                else:
+                    students_dict[selected_student] = students_dict.get(selected_student) + ', ' +\
+                                                      task[0].lower() + task[1:]
+
                 students_list.remove(selected_student)
 
     return students_dict
 
 
 """
-Print result depending on options
+Print result depending on settings
 """
 def print_result(turns_dict, students_dict, students_list):
     print('')
@@ -147,7 +159,12 @@ if __name__ == "__main__":
         turns_dict = assign_students_to_list(students_list, turns_dict)
 
     if ASSIGN_TASK is True:
-        task_list = create_tasks_list()
-        students_dict = distribute_tasks_between_students(task_list, students_list)
+        options_list = create_options_list()
+        task = 1
+        while task <= TOTAL_TASKS:
+            students_dict = distribute_tasks_between_students(options_list,
+                                                              *students_list,
+                                                              **students_dict)
+            task += 1
 
     print_result(turns_dict, students_dict, students_list)
